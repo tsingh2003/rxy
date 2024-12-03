375f508128f67da8eb1fa1e8a8843a9fee49032e
Tegh Singh <tsingh@wesleyan.edu>
2024-12-02 19:20:56 -0500

    wwwwwwwwwww

--- R/rxy_function.R
+++ R/rxy_function.R
@@ -1,3 +1,17 @@
+#'@title The base RYX Function
+#'@description Base function that gives the correlations between a dependent variable y and one or more independent x variables.
+#'@export
+#'@param data the dataset used
+#'@param y the dependent variable
+#'@param x the independent variable or variables
+#'@returns returns a chart with the correlations of y with x with their r values and p-values
+#'@import MASS 
+#'@examples
+#'
+#'data(Boston)
+#'summary(ryx(Boston, y="medv"))
+
+
 ryx <- function(data, y, x){
   if(missing(x)){
     x <- names(data)[sapply(data, class)=="numeric"]
